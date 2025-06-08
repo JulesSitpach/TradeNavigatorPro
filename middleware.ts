2a65ab25f800dc6174b3cbd98b857af9c84f395b
Onlook <git@onlook.com>
2025-06-08 11:15:10 -0600

    fix this issue as it was blocked

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // Check if there is any supported locale in the pathname
+  const pathname = request.nextUrl.pathname;
+  const pathnameIsMissingLocale = ["/en", "/es", "/fr"].every(
+    (locale) => !pathname.startsWith(`${locale}/`) && pathname !== locale,
+  );
+
+  // Redirect if there is no locale
+  if (pathnameIsMissingLocale) {
+    return NextResponse.redirect(new URL(`/en${pathname}`, request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api).*)"],
+};
